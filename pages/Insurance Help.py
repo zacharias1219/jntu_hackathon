d3d3af13c0bafe2a107af0f4ebbf2ceea34fa8c3
Richard Abishai <rickoshade1891@gmail.com>
2024-05-19 16:02:12 +0530

    insurance page

---
+++ pages/Insurance Help.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from utilities.icon import page_icon
+
+st.set_page_config(
+    page_title="Insurance Help",
+    layout="wide",
+    page_icon="📜",
+    initial_sidebar_state="collapsed",
+)
+
+st.header("📜 Insurance Help")
